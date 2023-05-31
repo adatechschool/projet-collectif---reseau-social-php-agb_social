44938715a3856f42e6340e0b4eff8f8489d78589
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-05-31 16:38:18 +0200

    Database access

--- Niveau1/Niveau1/mySql.php
+++ Niveau1/Niveau1/mySql.php
@@ -1,9 +1,12 @@
 <?php
+  $env = parse_ini_file(".env");
+  $dbpassword = $env["DB_PASSWORD"];
+  $login= $env["LOGIN"];
  /**
        * Etape 1: Ouvrir une connexion avec la base de donnée.
          */
         // on va en avoir besoin pour la suite
-        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+        $mysqli = new mysqli("localhost", $login, $dbpassword, "socialnetwork");
         //verification
         if ($mysqli->connect_errno)
         {
