972445d27caad8bcebc8312f54b318fc54734f2c
Gokuada <124284809+Gokuada@users.noreply.github.com>
2023-06-05 11:07:04 +0200

    modif news

--- Niveau1/Niveau1/news.php
+++ Niveau1/Niveau1/news.php
@@ -66,6 +66,7 @@
                 // si vous ne la comprenez pas c'est normal, passez, on y reviendra
                 $laQuestionEnSql = "
                     SELECT posts.content,
+                    posts.user_id,
                     posts.created,
                     users.alias as author_name,  
                     count(likes.id) as like_number,  
@@ -107,7 +108,7 @@
                         <h3>
                             <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address><?php echo $post['author_name']?></address>
+                        <address> <a href="wall.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name']?></a></address>
                         <div>
                             <p><?php echo $post['content']?></p>
                         </div>
