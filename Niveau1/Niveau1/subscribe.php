2522bc3d989576d6781d44bbc57cbb5ec6206461
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-06-06 11:14:03 +0200

    Modified subscribe & wall, correct SQL insertion

--- Niveau1/Niveau1/subscribe.php
+++ Niveau1/Niveau1/subscribe.php
@@ -8,8 +8,8 @@
                   ini_set('display_errors', 1);
                   ini_set('display_startup_errors', 1);
                   error_reporting(E_ALL);
-                $enCoursDeTraitement = isset($_POST['Follow']);
-                $userId=$_GET['user_id'];
+                $enCoursDeTraitement = isset($_POST['follow_php']);
+                $userId2 = intval($_GET['user_id']);
                 if ($enCoursDeTraitement) {
                     echo "<pre>" . print_r($_POST, 1) . "</pre>";
                 include "mySql.php";
@@ -17,7 +17,7 @@
                 $lInstructionSql = "INSERT INTO followers (id, followed_user_id, following_user_id)"
                     . "VALUES (NULL, "
                     . "'" . $_SESSION['connected_id'] . "', "
-                    . "'" . $userId . "'); "
+                    . "'" . $userId2 . "'); "
                     ;
 
                 $ok = $mysqli->query($lInstructionSql);
@@ -25,13 +25,13 @@
                 if (!$ok) {
                     echo "Votre requete n'a pas abouti : " . $mysqli->error;
                 } else {
-                    echo "Vous venez de follow  : " . $userId;
+                    echo "Vous venez de follow  : " . $userId2;
                    
                 }
             }
                 ?>
-                    <form action="subscribe.php" method="post">
-                    <input type='submit' value="Follow"> <label for="Follow">
+                    <form action="wall.php?user_id=<?php echo $userId2 ?>" method="post">
+                    <input type="submit" value="Follow" name="follow_php"> 
             </form>
         </article>
     </main>
