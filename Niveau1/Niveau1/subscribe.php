2b248f3f2d180942331cd2f3c682d86590b43f07
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-06-06 11:37:58 +0200

    Modified subscribe

--- Niveau1/Niveau1/subscribe.php
+++ Niveau1/Niveau1/subscribe.php
@@ -1,10 +1,7 @@
 <div id="wrapper">
-
     <main>
         <article>
-            
-            
-                <?php
+            <?php
                   ini_set('display_errors', 1);
                   ini_set('display_startup_errors', 1);
                   error_reporting(E_ALL);
